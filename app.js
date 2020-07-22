ed5a364cc6ed408c9e62d4e4fc99fd97611ef36c
AtiqaRehmat <66905432+AtiqaRehmat@users.noreply.github.com>
2020-07-23 01:14:43 +0500

    Update app.js

--- app.js
+++ app.js
@@ -1,3 +1,802 @@
+//                 Ch # 1 ALERT TASK 1
+ 
+ //alert("Error! please enter valid pasword");              
+
+
+
+ //                 Ch # 1 ALERT TASK 3
+
+//alert("Welcome to Js land..." +"\n"+ "Happy  codding!");                 
+
+
+
+ //                 Ch # 1 ALERT TASK 4
+
+//alert("Welcome to Js land...");                                 
+//alert("Happy  codding!");
+
+
+ //                 Ch # 1 ALERT TASK 5
+
+//document.write("Hello.... I can run Js throug my web browser's console");
+
+
+
+ //                 Ch # 2 VARIABLES FOR STRING TASK 1
+
+// var username = ("ATIQA   ");
+//alert(username);
+
+
+ //                 Ch # 2 VARIABLES FOR STRING TASK 2
+
+ //var name =("REHMAT");
+ //alert(name);
+ // alert(username + name);
+
+
+ //                 Ch # 2 VARIABLES FOR STRING TASK 3
+
+
+//var message=("Hello world");
+//alert(message);
+ 
+
+
+ //                 Ch # 2 VARIABLES FOR STRING TASK 4
+
+// var name =("Atiqa");
+//alert(name);
+
+//var age=("21 years old");
+//alert(age);
+
+//var qualification=("BS IT Undergraduate");
+//alert(qualification);
+
+
+
+
+ //                 Ch # 2 VARIABLES FOR STRING TASK 5
+
+//var line1=("PIZZA");
+//var line2=("PIZZ");
+//var line3=("PIZ");
+//var line4=("PI");
+//var line5=("P");
+//alert(line1+"\n"+line2+"\n"+line3+"\n"+line4+"\n"+line5);
+
+
+
+ //                 Ch # 2 VARIABLES FOR STRING TASK 6
+
+//var message =("My email address is : ");
+//var email =("atiqa@gimal.com");
+//alert(message+email);
+
+
+
+ //                 Ch # 2 VARIABLES FOR STRING TASK 7
+
+//var book = ("A smarter way to learn JavaScript ");
+//alert(book);
+
+
+
+
+ //                 Ch # 2 VARIABLES FOR STRING TASK 8
+
+//document.write("Yah! I can write HTML content through JavaScript");
+
+
+
+
+
+ //                 Ch # 2 VARIABLES FOR STRING TASK 9
+
+ //alert("“▬▬▬▬▬▬▬▬▬ஜ۩۞۩ஜ▬▬▬▬▬▬▬▬▬”");
+
+
+
+
+
+
+
+  //                 Ch # 3 VARIABLES FOR NUMBERS TASK 1
+
+//var age = ("21 years old");
+//alert(age);
+
+
+
+  //                 Ch # 3 VARIABLES FOR NUMBERS TASK 2
+
+ // var visit = ("you visit");
+ // visit =1++;
+ // alert(visit);
+
+
+
+   //                 Ch # 3 VARIABLES FOR NUMBERS TASK 3
+
+//var birthYear=("My birth year is 1998 <br> data type of my deaclared variable is number ");
+//document.write(birthYear);
+
+
+
+
+   //                 Ch # 3 VARIABLES FOR NUMBERS TASK 4
+
+ //  var visitorName=("<strong>Atiqa </strong>");
+//var productTitle=(" ordered");
+//var quantity=(" <strong> 5 T-shirt </strong>");
+//document.write(visitorName+ productTitle +  quantity + "(s) on XYZ Clothing store");
+
+
+
+
+   //                 Ch # 4 VARIABLES NAME LEGAL AND ILLEGAL TASK 1
+
+ //  var name = "atiqa";
+ //  var name = "atiqa";
+// var name = "atiqa";
+
+
+
+//                 Ch # 4 VARIABLES NAME LEGAL AND ILLEGAL TASK 2
+
+//var name1;
+//var asianCountry;
+//var tiTle;
+//var Roll_No;
+//var StudentNo1;
+
+
+
+//var 2name;
+//var @asianCountry;
+//var Roll No;
+//var 123;
+//var Class^no^3
+
+
+
+
+
+//                 Ch # 4 VARIABLES NAME LEGAL AND ILLEGAL TASK 3
+
+//var heading = ("<h1>Rules for naming JS variables</h1>");
+//document.write(heading);
+//var name = ("atiqa");
+//var age = ("21");
+//var qualification = ("bs IT undergraduate");
+//document.write("<h4>Write your name age and qualification "+name +" , "+age+" , "+qualification+" , " +"</h4>");
+//document.write("<h4> Variables must begin with a ______, ______ ,_____. For example $name, _name or name </h4>");
+//document.write("<h4> Variable names are case _________</h4>");
+//document.write("<h4>Variable names should not be JS _________ </h4>");
+
+
+
+
+
+//                 Ch # 5 MATH EXPRESSIONS  TASK 1
+
+//var num1 = 5, num2 = 3, sum;
+//sum=5+3;
+//document.write("sum of 5 + 3 is "+sum );
+
+
+
+
+//                 Ch # 5 MATH EXPRESSIONS  TASK 2
+
+//var num1 = 5, num2 = 3, sum;
+//sum=5+3;
+//sub=5-3;
+//div=5/3;
+//multi=5*3;
+//modula=5%3;
+//document.write("sum of 5 + 3 is "+sum +"<br>");
+//document.write("subtraction of 5 - 3 is "+sub+ "<br>");
+//document.write("division of 5 / 3 is "+div+ "<br>" );
+//document.write("multiplication of 5 X 3 is "+multi+ "<br>" );
+//document.write("modula of 5 % 3 is "+modula + "<br>");
+
+
+
+
+//                 Ch # 5 MATH EXPRESSIONS  TASK 3
+
+//var num1 = 5;
+//var num2;
+//num2=12%6;
+//document.write("<h2>Value after variable declaration is undefined </h2>");
+//document.write("<h4> Initial value : "+(num1++)+"</h4>");
+//document.write("<h4> value after increament is : "+(num1)+"</h4>");
+//document.write("<h4> value after addition is : "+(num1+7)+"</h4>");
+//document.write("<h4> value after decreament is : "+(num1+6)+"</h4>");
+//document.write("<h4> the reminder is : "+num2+"</h4>");
+
+
+
+
+
+
+//                 Ch # 5 MATH EXPRESSIONS  TASK 4
+
+//var OneticketPrice=600;
+//var  FiveticketPrice=5;
+//var total;
+//total=FiveticketPrice*OneticketPrice;
+//document.write("<h1>Total cost of buy 5 tickets to a movie is "+total+" PKR</h1>");
+
+
+
+
+
+//                 Ch # 5 MATH EXPRESSIONS  TASK 5
+
+//var i;
+//document.write("<h1><u>TABLE OF 7<u></h1>")
+//for( i=1; i<=10; i++)
+//{
+//document.write("7 "+" X "+(i)+" = "+(7*i)+"<br>");
+//}
+
+
+
+
+
+
+//                 Ch # 5 MATH EXPRESSIONS  TASK 6
+
+//var celsius, fahrenheit;
+//celsius=(70-32)*5/9;
+//fahrenheit=(25*9/5)+32;
+//document.write("25 celsius ="+fahrenheit+"<br>");
+//document.write("70 fahrenheit ="+celsius);
+
+
+
+
+//                 Ch # 5 MATH EXPRESSIONS  TASK 7
+
+//var item1Price=650;
+//var quantity1=3;
+//var item2Price=100;
+//var quantity2=7;
+//var shippingCharges=100;
+//var total;
+//total=(item1Price*3)+(item2Price*quantity2)+shippingCharges
+//document.write("Price of 1 item ="+item1Price+"<br>");
+//document.write("Quantity of 1 item ="+quantity1+"<br>");
+//document.write("Price of 2 item ="+item2Price+"<br>");
+//document.write("Quantity of 2 item ="+quantity2+"<br>");
+//document.write("Shipping charges ="+shippingCharges+"<br>");
+//document.write("Total cost of our order ="+total+"<br>");
+
+
+
+
+
+
+//                 Ch # 5 MATH EXPRESSIONS  TASK 8
+
+//var totalMarks=980, obtained=804, per;
+//per=(obtained/totalMarks)*100;
+//document.write("<h1>MARKSHEET</h1><br>")
+//document.write("Toatal marks = "+totalMarks+"<br>");
+//document.write("Obtained marks = "+obtained+"<br>");
+//document.write("Your percentage = "+per+"<br>");
+
+
+
+
+
+//                 Ch # 5 MATH EXPRESSIONS  TASK 9
+
+//var  USDollar = 104.80;
+//var   SaudiRiyal = 28;
+//sum=USDollar+SudiRiyal;
+//document.write("<h1>Currency In Pakistan</h1>");
+//document.write(sum);
+
+
+
+
+//                 Ch # 5 MATH EXPRESSIONS  TASK 10
+
+//var num=2+5*10/2;
+
+//document.write(num);
+
+
+
+
+
+//                 Ch # 5 MATH EXPRESSIONS  TASK 11
+
+//var currentyear=2016;
+//var birthYear=1992;
+//var age=(currentyear-birthYear);
+//document.write("<h1>AGE CALCULATOR</h1>");
+//document.write("<h3>CURRENT YEAR = "+currentyear+"</h3>");
+//document.write("<h3>BIRTH YEAR = "+birthYear+"</h3>");
+//document.write("<h3>AGE = "+age+"</h3>");
+
+
+
+
+//                 Ch # 5 MATH EXPRESSIONS  TASK 12
+
+//var pi=3.142;
+//var radius=20;
+//var cir,area;
+//cir=(2*pi*radius);
+//area=(pi*radius*radius);
+//document.write("<h1>The Geometrizer</h1>");
+//document.write("<h1>Radius of a circle = 20</h1>");
+//document.write("<h1>The circumferece = "+cir+"</h1>");
+//document.write("<h1>The area ="+area+"</h1>");
+
+
+
+
+
+//                 Ch # 5 MATH EXPRESSIONS  TASK 13
+
+//var snack=("choclate chip");
+//var age=("15");
+//var maxAge=("65");
+//var perDaySnack=("3");
+//var total;
+//total=(maxAge-age)*3;
+//document.write("<h1>The Lifetime Supply Calculator:</h1>");
+//document.write("<h3>favorite snack : "+snack+"</h3>");
+//document.write("<h3>current age : "+age+"</h3>");
+//document.write("<h3> estimated  max age : "+maxAge+"</h3>");
+//document.write("<h3> amount of snack per day : "+perDaySnack+"</h3>");
+//document.write("<h3> you will need  : "+perDaySnack+" "+snack+" to last until the ripe old age of " +maxAge+"</h3>");
+
+
+
+
+
+
+
+
+
+//                 Ch # 6-9 MATH EXPRESSIONS  TASK 1
+
+//var a = ("10");
+
+//document.write("<h1>RESULT</h1>");
+//document.write("<h2>The value of a = "+a+"</h2>");
+//document.write("<h2>_________________________</h2>");
+//document.write("<h2>The value of ++a = "+(++a)+"</h2>");
+//document.write("<h2>Now the value of a = "+a+"</h2><br><br>");
+//document.write("<h2>The value of a++ = "+(a++)+"</h2>");
+//document.write("<h2>The value of a = "+a+"</h2><br><br>");
+//document.write("<h2>The value of --a = "+(--a)+"</h2>");
+//document.write("<h2>The value of a = "+a+"</h2><br><br>");
+//document.write("<h2>The value of a-- = "+(a--)+"</h2>");
+//document.write("<h2>The value of a = "+a+"</h2><br><br>");
+
+
+
+
+
+//                 Ch # 6-9 MATH EXPRESSIONS  TASK 2
+
+//var a = 2, b = 1;
+//var result = --a - --b + ++b + b--;
+//document.write("a is = "+a+"<br>");
+//document.write("b is = "+b+"<br>");
+//document.write("Result is = "+result);
+
+
+
+
+
+//                 Ch # 6-9 MATH EXPRESSIONS  TASK 3
+
+//var name = prompt("What is your name");
+//alert(name);
+
+
+
+//                 Ch # 6-9 MATH EXPRESSIONS  TASK 5
+
+//var num = prompt("Enter table number");
+//var num1=5;
+//for(var i=1; i<=10; i++)
+//{
+//document.write(num+" X "+i+" = "+(num*i)+"<br><br>");
+//}
+
+
+ // for(var j=1; j<=10; j++)
+//{
+//document.write(num1+" X "+j+" = "+(num1*j)+"<br><br>");
+//}
+
+
+
+
+
+
+
+//                 Ch # 6-9 MATH EXPRESSIONS  TASK 6
+
+//var eng=54;
+//var urdu=48;
+//var math=54;
+//var totalMarks=(eng+urdu+math);
+//var perc=(totalMarks/300)*100;
+//document.write("<strong>|  SUBJECT  |  TOTAL MARKS  |  OBTAINED MARKS  |  PERCENTAGE  |</strong>  ");
+//document.write("<br><strong>--------------------------------------------------------------------------------------------</strong>  ");
+//document.write("<br><br>|  Engl  |  100  |"+"  "+eng+"  |  54%  |");
+//document.write("<br><br>|  Urdu  |  100  |"+"  "+urdu+"  |  48%  |");
+//document.write("<br><br>|  Math  |  100  |"+"  "+math+"  |  54%  |");
+//document.write("<br><br>|  Total  |  300  |"+" | "+totalMarks+"  |  "+perc + "%  |");
+
+
+
+
+
+//                 Ch # 9-11 USER INPUT & CONDITIONAL STATEMENT   TASK 1
+
+//var city=prompt("Enter city name");
+//if (city === 'karachi')
+//{
+ // alert("Wellcome to city of lights");
+//}
+
+
+
+
+//                 Ch # 9-11 USER INPUT & CONDITIONAL STATEMENT   TASK 2
+
+//var gender=prompt("enter your gender");
+//if(gender=== 'male')
+//{
+//alert("Good morning sir");
+
+//}
+//else if(gender=== 'female')
+//{
+ // alert("Good morning mam");
+//}
+//else
+//{
+//alert("enter right gender");
+//}
+
+
+
+
+//                 Ch # 9-11 USER INPUT & CONDITIONAL STATEMENT   TASK 3
+
+//document.write("<table border=1px align=center bgcolor=skyblue><tr><th width=200px>SIGNAL COLOR</th><th width=200px>MASSAGE</th></tr></table>");
+//document.write("<table border=1px align=center ><tr><td width=200px align=center>Red</td><td width=200px align=center>Must stop</td></tr></table>");
+//document.write("<table border=1px align=center bgcolor=skyblue><tr><td width=200px align=center>Yellow</td><td width=200px align=center>Ready to move</td></tr></table>");
+//document.write("<table border=1px align=center ><tr><td width=200px align=center>Green</td><td width=200px align=center>Move now</td></tr></table>");
+
+
+
+
+
+
+//                 Ch # 9-11 USER INPUT & CONDITIONAL STATEMENT   TASK 4
+
+//var fuel = prompt("how much remaining fuel in your car ");
+//if(fuel < '0.25')
+//{
+//alert("Please refill the fuel in your car");
+//}
+//else{
+//alert("fuel is full");
+//}
+
+
+
+//                 Ch # 9-11 USER INPUT & CONDITIONAL STATEMENT   TASK 5
+
+//        a
+
+//var a = 4;
+//if (++a === 5){
+//alert("given condition for variable a is true");
+//}
+
+
+//        b
+
+//var b = 82;
+//if (b++ === 83){
+//alert("given condition for variable b is true");
+//}
+
+//        c
+
+ //var c = 12;
+//if (c++ === 13){
+//alert("condition 1 is true");
+//}
+//if (c === 13){
+//alert("condition 2 is true");
+//}
+//if (++c < 14){
+//alert("condition 3 is true");
+//}
+//if(c === 14){
+//alert("condition 4 is true");
+//}
+
+//        d
+
+//var materialCost = 20000;
+//var laborCost = 2000;
+//var totalCost = materialCost + laborCost;
+//if (totalCost === laborCost + materialCost){
+//alert("The cost equals");
+//}
+
+//        e
+
+ //if (true){
+ // alert("True");
+  //}
+  //if (false){
+   // alert("False");
+   // }
+  
+  //        f
+
+  //if("car" < "cat"){
+   // alert("car is smaller than cat");
+   // }
+  
+
+
+
+
+
+
+//                 Ch # 9-11 USER INPUT & CONDITIONAL STATEMENT   TASK 6
+
+//document.write("<h1 align=center><u>MARKSHEET<u></h1>")
+//document.write("<h2 align=center><u>Total marks = 300<u></h2>")
+//var eng = +prompt("Enter english marks");
+//var urdu = +prompt("Enter urdu marks");
+//var math = +prompt("Enter math marks");
+//var obt;
+//var total=300;
+//obt=(eng+urdu+math);
+//document.write("<h2 align=center><u>Obtained marks = "+obt+"<u></h2>")
+//var per;
+//per = (obt/total)*100;
+//document.write("<h2 align=center><u>Percentage = "+per+"%<u></h2>");
+//if (per >= '80' )
+//{
+//document.write("<h2 align=center><u>Grade = A-1 <u></h2>");
+//document.write("<h2 align=center><u>Remarks = Excellent <u></h2>");
+//}
+//else if (per >= '70' )
+//{
+//document.write("<h2 align=center><u>Grade = A <u></h2>");
+//document.write("<h2 align=center><u>Remarks = Good <u></h2>");
+//}
+//else if (per >= '60' )
+//{
+//document.write("<h2 align=center><u>Grade = B <u></h2>");
+//document.write("<h2 align=center><u>Remarks = You need to improve <u></h2>");
+//}
+//else if (per < '60' )
+//{
+//document.write("<h2 align=center><u>Grade = Fail <u></h2>");
+//document.write("<h2 align=center><u>Remarks = Sorry <u></h2>");
+//}
+
+
+
+
+
+//                 Ch # 9-11 USER INPUT & CONDITIONAL STATEMENT   TASK 7
+
+//var num= +prompt("write any number ");
+
+//if(num == '5')
+//{
+ // alert("“Bingo! Correct answer");
+//}
+
+
+//else  (num > 5)
+//{
+ // alert("“Close enough to the correct answer");
+//}
+
+//                 Ch # 9-11 USER INPUT & CONDITIONAL STATEMENT   TASK 8
+
+//var num = +prompt("Enter any number ");
+//var result;
+//result=(num%3);
+
+//if (result === 0)
+//{
+//alert("divisible by 3");
+//}
+//else
+//{
+//alert(num+" is not divisible by 3");
+//}
+
+
+//                 Ch # 9-11 USER INPUT & CONDITIONAL STATEMENT   TASK 9
+
+//var num = +prompt("Enter any number ");
+
+
+//if (num%2 === 1)
+//{
+//alert(num+" is Odd number");
+//}
+//else 
+//{
+//alert(num+" is even number");
+//}
+
+//                 Ch # 9-11 USER INPUT & CONDITIONAL STATEMENT   TASK 10
+
+//var temp = prompt("enter temperature");
+//if(temp >  40)
+//{
+  //alert("It is too hot outside");
+//}
+
+//else if(temp >  30)
+//{
+ // alert("The Weather today is Normal ");
+//}
+
+//else if(temp >  20)
+//{
+ // alert("Today’s Weather is cool. ");
+//}
+
+//else if(temp >  10)
+//{
+ // alert("OMG! Today’s weather is so Cool. ");
+//}
+
+//                 Ch # 9-11 USER INPUT & CONDITIONAL STATEMENT   TASK 11
+
+//var num1= +prompt("enter first number ");
+//var num2= +prompt("enter second number ");
+//var op= prompt("enter operation ");
+//if(op === '+')
+//{
+ // alert(num1+" + "+num2+" = "+(num1+num2));
+//}
+//else if(op === '-')
+//{
+ // alert(num1+" - "+num2+" = "+(num1-num2));
+//}
+//else if(op === '*')
+//{
+//  alert(num1+" X "+num2+" = "+(num1*num2));
+//}
+//else if(op === '/')
+//{
+ // alert(num1+" / "+num2+" = "+(num1/num2));
+//}
+//else if(op === '%')
+//{
+ // alert(num1+" % "+num2+" = "+(num1%num2));
+//}
+
+
+
+
+
+
+
+//                 Ch # 12-13 IF…ELSE & ELSE IF STATEMENT,TESTING SET OF CONDITIONS    TASK 1
+
+//var alpha = prompt("Enter any one alphabet ");
+//alert(alpha.charCodeAt());
+
+
+//                 Ch # 12-13 IF…ELSE & ELSE IF STATEMENT,TESTING SET OF CONDITIONS    TASK 2
+
+//var num1 = +prompt("Enter number "); 
+//var num2 = +prompt("Enter number ");
+
+//if(num1 > num2)
+//{
+//alert(num1+" is grater value");
+//}
+
+//else if(num1 < num2)
+//{
+//alert(num2+" is grater value ");
+//}
+//else if(num1 = num2)
+//{
+//alert(num1+" is equal  to "+num2);
+//}
+
+
+//                 Ch # 12-13 IF…ELSE & ELSE IF STATEMENT,TESTING SET OF CONDITIONS    TASK 3
+
+//var num= +prompt("Enter number");
+//if(num <= 0)
+//{
+//alert(num+" is Negative number");
+//}
+
+//else if(num > 0)
+//{
+//alert(num+" is Positive number");
+//}
+
+
+
+//                 Ch # 12-13 IF…ELSE & ELSE IF STATEMENT,TESTING SET OF CONDITIONS    TASK 4
+
+//var char = prompt("Enter any one  character ");
+//if(char === 'a' || char === 'e' || char === 'i' ||  char === 'o' ||  char === 'u'   )
+//{
+//alert("Yes your character is vowel ");
+//}
+//else
+//{
+ // alert("Your character is consonant");
+//}
+
+//                 Ch # 12-13 IF…ELSE & ELSE IF STATEMENT,TESTING SET OF CONDITIONS    TASK 5
+
+//var password=("mirpurkhas");
+
+//var pas = prompt("enter your password");
+//if(pas === password)
+//{
+//alert("Correct! The password you entered matches the original password");
+//}
+//else if(pas != password)
+//{
+//alert("Incorrect password");
+//}
+//else if (pas ===  unescape ) 
+//{
+//alert(" Please enter your password");
+//}
+
+
+
+//                 Ch # 12-13 IF…ELSE & ELSE IF STATEMENT,TESTING SET OF CONDITIONS    TASK 6
+
+//var greeting=+prompt("enter time");
+//greeting=hour;
+//var hour = 13;
+//if (hour < 18) {
+//alert("Good day");
+//}
+//else
+//{
+//alert("Good evening") ;
+//}
+
+//                 Ch # 12-13 IF…ELSE & ELSE IF STATEMENT,TESTING SET OF CONDITIONS    TASK 7
+
+
+
+
+
+
+
+
 
 //    ================ CH__NO__(38_to_44)__TASK_(1) ======================
 
